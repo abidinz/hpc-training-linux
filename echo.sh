26dcbe1a946fb11e6cf4536a7ae87aa3feb0c7c6
Tomi Ilham <tomi.ilham@gmail.com>
2024-02-26 20:31:43 +0700

    Menambahkan contoh file untuk jenis-jenis pemrograman shell

---
+++ echo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Ini merupakan sebuah prosedur shell yang sangat sederhana"
+echo "dibuat dengan perintah dasar echo"
+echo "dan tiga perintah dasar lainnya"
+echo
+ps
+echo
+who
+echo
+ls
